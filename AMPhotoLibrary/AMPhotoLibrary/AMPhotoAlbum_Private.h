c76075f8b83095bea3ef4bb5789fef071f98369b
ray <brightmar@gmail.com>
2016-07-15 10:51:11 +0800

    code fix

---
+++ AMPhotoLibrary/AMPhotoLibrary/AMPhotoAlbum_Private.h
@@ -0,0 +1,13 @@
+//
+//  AMPhotoAlbum_private.h
+//  QBImagePicker
+//
+//  Created by ray on 16/7/14.
+//  Copyright © 2016年 Katsuma Tanaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMPhotoAlbum_private : NSObject
+
+@end
---
+++ AMPhotoLibrary/AMPhotoLibrary/AMPhotoAsset_Private.h
@@ -0,0 +1,13 @@
+//
+//  AMPhotoAsset_Private.h
+//  QBImagePicker
+//
+//  Created by ray on 16/7/14.
+//  Copyright © 2016年 Katsuma Tanaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMPhotoAsset_Private : NSObject
+
+@end
